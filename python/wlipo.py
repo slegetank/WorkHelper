6c49175d51f77c40d0753e8e95a2aa18aa342861
Shuai Zhao <shuai.zhao@wiimu.com>
2016-08-29 13:21:54 +0800

    wlipo multi target bug fix

--- python/wlipo.py
+++ python/wlipo.py
@@ -30,11 +30,11 @@ def getLibTargets(projectFilePath):
         if product:
             productName = product.group()
             if productName.endswith('.a'):
-                retArr.append(targetName)
+                retArr.append({'target':targetName, 'product':productName})
 
     return retArr
 
-def generateLib(projectPath, targetName):
+def generateLib(projectPath, targetName, productName):
     BUILD_DIR = os.path.join(projectPath, 'build')
     OUTPUT_DIR = os.path.expanduser('~/Desktop')
 
@@ -42,8 +42,8 @@ def generateLib(projectPath, targetName):
     os.system('xcodebuild clean -configuration Release')
     os.system('xcodebuild -target %s ONLY_ACTIVE_ARCH=NO -configuration Release -sdk iphoneos BUILD_DIR=\"%s\"' % (targetName, BUILD_DIR))
     os.system('xcodebuild -target %s ONLY_ACTIVE_ARCH=NO -configuration Release -sdk iphonesimulator BUILD_DIR=\"%s\"' % (targetName, BUILD_DIR))
-    os.system('lipo -create -output \"%s/lib%s.a\" \"%s/Release-iphoneos/lib%s.a\" \"%s/Release-iphonesimulator/lib%s.a\"' % (OUTPUT_DIR, targetName, BUILD_DIR, targetName, BUILD_DIR, targetName))
-    os.system('lipo -info \"%s/lib%s.a\"' % (OUTPUT_DIR, targetName))
+    os.system('lipo -create -output \"%s/%s\" \"%s/Release-iphoneos/%s\" \"%s/Release-iphonesimulator/%s\"' % (OUTPUT_DIR, productName, BUILD_DIR, productName, BUILD_DIR, productName))
+    os.system('lipo -info \"%s/%s\"' % (OUTPUT_DIR, productName))
     os.system('rm -rf %s' % BUILD_DIR)
     os.system('open %s' % OUTPUT_DIR)
     print '\xF0\x9f\x8d\xba  Cmd execution finished. Have fun.'
@@ -71,12 +71,14 @@ if __name__ == '__main__':
                 print 'Found no lib target. Abort.'
                 sys.exit(0)
             elif len(targets) == 1:
-                generateLib('%s/../' % projectFilePath, targets[0])
+                generateLib('%s/../' % projectFilePath, targets[0]['target'], targets[0]['product'])
             else:
                 print 'Found multi targets:\n'
                 for i in range(len(targets)):
-                    print '%d %s' % (i, target)
+                    targetName = targets[i]['target']
+                    print '%d %s' % (i, targetName)
 
                 choice = raw_input('which do you want to build?')
                 if int(choice) < len(targets):
-                    generateLib('%s/../' % projectFilePath, targets[int(choice)])
+                    target = targets[int(choice)]
+                    generateLib('%s/../' % projectFilePath, target['target'], target['product'])
